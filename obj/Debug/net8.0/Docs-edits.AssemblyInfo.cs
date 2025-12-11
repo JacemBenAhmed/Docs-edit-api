eda3d300c4ea30715c9e8012329a81f7b47b08da
JacemBenAhmed <jacem0987654321@gmail.com>
2025-12-11 15:17:04 +0100

    add / route (health)

--- obj/Debug/net8.0/Docs-edits.AssemblyInfo.cs
+++ obj/Debug/net8.0/Docs-edits.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Docs-edits")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c21a771da2001f270c838dc301e53b33b2fba066")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+749f674e1c47dda84a1b52c139d1d7dc205e910f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Docs-edits")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Docs-edits")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
